68bc50a59edf5621d6d76f3a7e0e803f6e7b3325
Scott Lahteine <sourcetree@thinkyhead.com>
2016-04-25 18:24:12 -0700

    Call report_current_position after M206 / M428

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -5208,7 +5208,9 @@ inline void gcode_M206() {
     if (code_seen('T')) set_home_offset(X_AXIS, code_value()); // Theta
     if (code_seen('P')) set_home_offset(Y_AXIS, code_value()); // Psi
   #endif
+
   sync_plan_position();
+  report_current_position();
 }
 
 #if ENABLED(DELTA)
@@ -5927,6 +5929,7 @@ inline void gcode_M428() {
 
   if (!err) {
     sync_plan_position();
+    report_current_position();
     LCD_MESSAGEPGM(MSG_HOME_OFFSETS_APPLIED);
     #if HAS_BUZZER
       buzz(200, 659);
