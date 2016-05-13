b75a0175b4c5982048a78f63f2f8efbc82cf45d0
esenapaj <esenapaj@users.noreply.github.com>
2016-05-13 21:10:23 +0900

    Enclose all #error strings with apostrophes

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -140,7 +140,7 @@
 #elif MB(SAINSMART_2IN1)
   #include "pins_SAINSMART_2IN1.h"
 #else
-  #error Unknown MOTHERBOARD value set in Configuration.h
+  #error "Unknown MOTHERBOARD value set in Configuration.h"
 #endif
 
 // Define certain undefined pins
